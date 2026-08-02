28842816fc3460c90b3203620214e47a9e35d9b8
Muhammad Zahid <mzahid@gmail.com>
2026-08-02 00:27:03 -0500

    fixed back to old NET SDK 10.0.202

--- QSearch/QSearch.xaml.cs
+++ QSearch/QSearch.xaml.cs
@@ -137,18 +137,17 @@ public partial class QSearch : ContentPage, IOnPageKeyDown
 #else
         currentLanguage = new LangDetection().LanguageDetected;
 #endif
-        // if (currentLanguage == "en" && selectedLanguage != 1)
-        // {
-        //     await DisplayAlertAsync("Q-Search", "Please choose English translation for search!", "Ok");
-        //     return;
-        // }
-        // else if (currentLanguage == "ur" && selectedLanguage != 2)
-        // {
-        //     await DisplayAlertAsync("Q-Search", "Please choose Urdu translation for search!", "Ok");
-        //     return;
-        // }
-        
-        selectedLanguage = currentLanguage == "en" ? 1 : 2;
+        if (currentLanguage == "en" && selectedLanguage != 1)
+        {
+            await DisplayAlertAsync("Q-Search", "Please choose English translation for search!", "Ok");
+            return;
+        }
+        else if (currentLanguage == "ur" && selectedLanguage != 2)
+        {
+            await DisplayAlertAsync("Q-Search", "Please choose Urdu translation for search!", "Ok");
+            return;
+        }
+       
         progress.ShowProgress();
         await Task.Delay(50);
         switch(currentLanguage)
